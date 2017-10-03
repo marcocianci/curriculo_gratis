32f5c44316dbee1170e03fddf901e3c36b2af1d9
Lucas Lima <lucasf.lima@msn.com>
2017-10-03 01:54:48 -0300

    update text

--- src/components/pages/Donate.vue
+++ src/components/pages/Donate.vue
@@ -4,7 +4,7 @@ div#donate
     h1 Doações
     p Esse web apliticativo é totalmente gratuito, sendo assim você não tem nenhuma obrigação financeira com ele.
     p Porém, ele é mantido online para ajudar outras pessoas assim como deve ter ajudado você.
-    p Esse custo de manter o projeto online e evoluiar o seu desenvolvimento virá dos fundos de doações que recebemos.
+    p Esse custo de manter o projeto online e evoluir o seu desenvolvimento virá dos fundos de doações que recebemos.
     p Se você sentir que precisar ajudar com esse projeto de alguma forma simbólica ou deseja expressar algum comentário, deixe sua contribuição no formulário logo a seguir
   br
   main.app.animate_app_intro
