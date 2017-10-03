78c61e9c382e6b1d1257b3c66dfecf452184eafa
Lucas Lima <lucasf.lima@msn.com>
2017-10-02 23:43:39 -0300

    adsense include

--- src/components/Advertising.vue
+++ src/components/Advertising.vue
@@ -1,7 +1,11 @@
 <template lang="pug">
-article.anuncio.display-n-print
-  h1 Neste espaço contém uma publicidade
-  p Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  article.anuncio.display-n-print
+    h1 Neste espaço contém uma publicidade
+    ins(class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-4080310689684959"
+         data-ad-slot="1351507400"
+         data-ad-format="auto")
 </template>
 
 <script>
